b112ed628dcab023f5c47dbb7e6f4fed3a9c7e7f
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-05-04 22:01:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+// import Table from './Component/Table';
 function App() {
+  const [apiData, setApiData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+      const data = await response.json();
+
+      setApiData(data)
+    }
+    getData();
+  }, [])
+  console.log(apiData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <table className='table'>
+        <thead>
+          <tr>
+         
+            <th className='s_no'>S.No</th>
+            <th className='name'>Name</th>
+            <th className='post'>Post Id</th>
+            <th className='email'>Email</th>
+            <th className='body'>Body</th>
+           
+          </tr>
+        </thead>
+      </table>
+      {apiData.map((d) =>
+    
+        <tbody key={d.id}>
+          <tr>
+          <td className='s_no'>
+              {d.id}
+            </td>
+            <td className='name'>
+              {d.name}
+            </td>
+            <td className='post'>
+              {d.postId}
+            </td>
+            <td className='email'>
+              {d.email}
+            </td>
+            <td className='body'>
+              {d.body}
+            </td>
+          </tr>
+        </tbody>
+     
+
+      )}
     </div>
   );
 }
---
+++ src/Component/Table.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Table = ({data}) => {
+  return (
+      <div>
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Post Id</th>
+            <th>Email</th>
+            <th>Body</th>
+        </tr>
+    </thead>
+</table>
+      </div>
+    
+  )
+}
+
+export default Table
